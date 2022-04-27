4eaadd7e83a079e30ca88e7dcbe6b0e5203cf935
deykerag <deykergil28@gmail.com>
2022-04-27 12:15:09 -0400

    [MOD] avances

--- crm_label_competitors/__manifest__.py
+++ crm_label_competitors/__manifest__.py
@@ -25,9 +25,6 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-        'views/crm_competitors.xml',
         'views/crm_add_menu_competitors.xml',
     ],
     # only loaded in demonstration mode
