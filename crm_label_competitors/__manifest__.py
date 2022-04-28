29cd839c9576f1592a3adb3a161d59ab9fea0746
deykerag <deykergil28@gmail.com>
2022-04-28 12:46:05 -0400

    [ADD][crm_lead_lost][crm_label_competitors]
       Incidence nbr./s.: ODOO-3742.
            - Add wizards/crm_lead_lost.py file
            - Add wizards/crm_lead_lost.xml file
            - Add models/crm_label_competitors.py file
            - Views and translations

--- crm_label_competitors/__manifest__.py
+++ crm_label_competitors/__manifest__.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "crm_label_competitors",
+
+    'name': "CRM Labels competitors",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
+        Module to add field of competence to CRM losses
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'description': """
+        Add competitors for CRM module
+    """,
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    "author": "Calyx Servicios S.A.",
+    "maintainers": [""],
+    "website": "http://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "version": "13.0.1.0.0",
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'crm'],
@@ -27,9 +26,6 @@
         'security/ir.model.access.csv',
         'views/crm_add_menu_competitors.xml',
         'wizards/crm_lead_lost_custom.xml'
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    ]
+
 }
