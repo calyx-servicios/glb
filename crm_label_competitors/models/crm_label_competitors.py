d430677ec0d455f47b078436c02da46ed1ad2b9a
deykerag <deykergil28@gmail.com>
2022-04-28 10:15:51 -0400

    [FIX] Fix de registro de competidores

--- crm_label_competitors/models/crm_label_competitors.py
+++ crm_label_competitors/models/crm_label_competitors.py
@@ -6,5 +6,6 @@ from odoo import models, fields, api
 class CrmLabelCompetitors(models.Model):
     _name = 'crm.label.competitors.registry'
     _description = 'Model registry of competitors'
+    _rec_name = 'name_competitor'
 
     name_competitor = fields.Char('Nombre de competidor')
