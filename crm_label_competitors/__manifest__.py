dfe177ea03a9fdaec930f5e58bf782d21ab639de
cristian <crisdparadiso@gmail.com>
2022-05-05 18:33:45 -0300

    [ADD][crm_label_competitors]ODOO-3742

---
+++ crm_label_competitors/__manifest__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+
+    'name': "CRM Labels competitors",
+
+    'summary': """
+        Module to add field of competence to CRM losses
+    """,
+
+    'description': """
+        Add competitors for CRM module
+    """,
+
+    "author": "Calyx Servicios S.A.",
+
+    "maintainers": ["DeykerGil","PerezGabriela"],
+    "website": "http://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "version": "13.0.1.0.0",
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'crm'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/crm_add_menu_competitors.xml',
+        'views/crm_views.xml',
+        'wizards/crm_lead_lost_custom.xml'
+    ]
+
+}
---
+++ crm_label_competitors/models/crm_label_competitors.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, _
+
+
+class CrmLabelCompetitors(models.Model):
+    _name = 'crm.label.competitors.registry'
+    _description = _('Model registry of competitors')
+    _rec_name = 'name_competitor'
+
+    name_competitor = fields.Char(_('Name competitor'))
