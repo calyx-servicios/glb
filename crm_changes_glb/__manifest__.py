cbf76ef2d2c891e724b7ba51d07c10386efeb01b
deykerag <deykergil28@gmail.com>
2022-05-26 20:44:30 -0400

    [ODOO-3824][ADD][crm_changes_glb] 	- Add module crm_changes_glb for GLB customizations and changes

---
+++ crm_changes_glb/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+
+    'name': "CRM Changes GLB",
+
+    'summary': """
+        CRM - Changes to GLB project 
+    """,
+
+    'description': """
+        Module to add and customize general GLB changes.
+    """,
+
+    "author": "Calyx Servicios S.A.",
+
+    "maintainers": ["DeykerGil"],
+    "website": "http://odoo.calyx-cloud.com.ar/",
+    "license": "AGPL-3",
+    "category": "Custom",
+    "version": "13.0.1.0.0",
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'crm'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/crm_team_views.xml',
+        # 'views/res_users_views.xml'
+    ]
+
+}
