2ba4a36a608f30a582be38193ea6f72c0cdc8baf
deykerag <deykergil28@gmail.com>
2022-05-31 14:55:21 -0400

    [ODOO-3762][ODOO-3877][UPD][crm_invoicing_goals]

--- crm_invoicing_goal_liters/models/res_users.py
+++ crm_invoicing_goal_liters/models/res_users.py
@@ -1,8 +1,19 @@
-from odoo import models, fields, _
+from odoo import models, fields, api, _
 
 
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    planned_liters = fields.Integer(_("Planned Liters")) #Litros planificados
-    current_liters = fields.Integer(_("Current Liters")) #Litros actuales
+    """ Bring liters of sales orders that are of the "Fuel" category and belong to the team member. """
+    def _compute_current_liters(self):
+        values = []
+        sale_orders = self.env['sale.order'].search([('user_id','=', self.id), ('order_line.product_id.categ_id.name', '=', 'Combustibles')])
+        
+        for so in sale_orders:
+            for line in so.order_line:
+                values.append(line.product_uom_qty)
+
+        self.current_liters = sum(values)
+    
+    current_liters = fields.Float(_("Current Liters"),compute=_compute_current_liters) #Litros actuales
+    planned_liters = fields.Float(_("Planned Liters")) #Litros planificados
