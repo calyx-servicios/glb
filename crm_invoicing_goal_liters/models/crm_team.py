75e06b32b84e9bdf5193d65d916c5e352f9a1883
deykerag <deykergil28@gmail.com>
2022-05-31 14:55:21 -0400

    [ODOO-3762][ODOO-3877][UPD][crm_invoicing_goals]

--- crm_invoicing_goal_liters/models/crm_team.py
+++ crm_invoicing_goal_liters/models/crm_team.py
@@ -5,6 +5,16 @@ from odoo import models, fields, _
 class CrmTeam(models.Model):
     _name = 'crm.team'
     _inherit = ['mail.alias.mixin', 'crm.team']
+    
+    """ Bring liters from each team member for the total actual liters."""
+    def _compute_current_liters_for_team(self):
+        values = []
+        for member in self.member_ids:
+            if member[0].current_liters:
+                values.append(member[0].current_liters)
+                
+        self.current_liters = sum(values)
+        
 
-    current_liters = fields.Integer(_("Current Liters"))
+    current_liters = fields.Integer(_("Current Liters"), compute=_compute_current_liters_for_team)
 
