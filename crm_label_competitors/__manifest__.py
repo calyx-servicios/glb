22a76031e783adbd855e6a0196dafa7a5bcadd68
deykerag <deykergil28@gmail.com>
2022-06-04 21:11:22 -0400

    [ODOO-3824][UPD][crm_label_competitors] 	- Removed comments needed for crm_label_competitors module

--- crm_label_competitors/__manifest__.py
+++ crm_label_competitors/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 {
 
     'name': "CRM Labels competitors",
@@ -19,10 +18,8 @@
     "category": "Custom",
     "version": "13.0.1.0.0",
 
-    # any module necessary for this one to work correctly
     'depends': ['base', 'crm'],
 
-    # always loaded
     'data': [
         'security/ir.model.access.csv',
         'views/crm_add_menu_competitors.xml',
