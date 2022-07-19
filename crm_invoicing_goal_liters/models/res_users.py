ae612a32ad68fe0e7e4af6d974cf51b4ed217536
deykerag <deykergil28@gmail.com>
2022-07-19 15:45:00 -0400

    [ODOO-4039][UPD][crm_invoicing_goal_liters]       - Added the cron for monthly calculation       - Added month to save monthly records of invoicing goals       - Hide current_liters field for sales team view       - Translations

--- crm_invoicing_goal_liters/models/res_users.py
+++ crm_invoicing_goal_liters/models/res_users.py
@@ -9,8 +9,8 @@ class ResUsers(models.Model):
     """ Bring liters of sales orders that are of the "Fuel" category and belong to the team member. """
     def _compute_current_liters(self):
         values = []
-        sale_orders = self.env['sale.order'].search([('user_id','=', self.id), ('order_line.product_id.categ_id.name', '=', 'Combustibles')])
-        
+        sale_orders = self.env['sale.order'].search([('user_id','=', self.id), ('order_line.product_id.categ_id.name', '=', 'Combustibles'), ('state', '=', 'logistics_auth')])
+
         for so in sale_orders:
             for line in so.order_line:
                 values.append(line.product_uom_qty)
@@ -19,22 +19,25 @@ class ResUsers(models.Model):
     
     current_liters = fields.Float("Current Liters",compute=_compute_current_liters)
     planned_liters = fields.Float("Planned Liters")
-
-    monthly_records_ids = fields.One2many('crm.team.monthtl.records', 'res_user_id', string='Monthly records')
+    monthly_records_ids = fields.One2many('crm.team.monthly.records', 'res_user_id', 'Monthly records')
     
     def cron_monthly_calculation_ig(self):
         records = self.env['res.users'].search([])
-        monthly_records_model = self.env['crm.team.monthtl.records']
-        values_so = []
+        monthly_records_model = self.env['crm.team.monthly.records']
+        ids = [] 
         date = datetime.datetime.now() + dateutil.relativedelta.relativedelta(months=-1)
-        for user in records:
-            for so in user.sale_order_ids.search([('user_id', '=', user.id)]):
-                for line in so.order_line:
-                    values_so.append(line.product_uom_qty)
 
-            monthly_records_model.create({
-                'registered_month': date ,
-                'res_user_id': user.id,
-                'planned_liters' : user.planned_liters,
-                'current_liters' : sum(values_so)
-            })
+        for user in records:
+            user._compute_current_liters()
+            if not user.id in ids:
+                monthly_records_model.create({
+                    'registered_month': date ,
+                    'res_user_id': user.id,
+                    'planned_liters' : user.planned_liters,
+                    'current_liters' : user.current_liters
+                })
+                user.write({
+                    'planned_liters' : 0
+                })
+
+                ids.append(user.id)
