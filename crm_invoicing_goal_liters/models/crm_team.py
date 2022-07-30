bd183142bc3aee569babb6ccc84463fd27f17157
deykerag <deykergil28@gmail.com>
2022-07-30 13:38:23 -0400

    [ODOO-4264][UPD][crm_invoicing_goal_liters] 	- Cron to goals monthly for team sale

--- crm_invoicing_goal_liters/models/crm_team.py
+++ crm_invoicing_goal_liters/models/crm_team.py
@@ -1,8 +1,6 @@
-# -*- coding: utf-8 -*-
-from psutil import users
+import datetime
+import dateutil.relativedelta
 from odoo import models, fields, _
-
-
 class CrmTeam(models.Model):
     _name = 'crm.team'
     _inherit = ['mail.alias.mixin', 'crm.team']
@@ -16,22 +14,29 @@ class CrmTeam(models.Model):
                 
         self.current_liters = sum(values)
         
-
     team_monthly_records_ids = fields.One2many('crm.team.monthly.records', 'team_id', 'Monthly records for sales team')
     current_liters = fields.Integer(_("Current Liters"), compute=_compute_current_liters_for_team)
 
     def cron_monthly_calculation_ig_team(self):
         team_ids = []
-        users = []
+        total_planned_ltr = []
+        total_current_ltr = []
 
         for team in self.search([]):
             if not team.id in team_ids:
-                users.append(self.env['res.users'].search([('id', 'in', team.member_ids.ids)]))
-                team.team_monthly_records_ids = [(6,0,{
-                    'team_id': team.id,
-                    'planned_liters' : sum([user.planned_liters for user in users])
-                })]
-                team_ids.append(team.id)
-        
+                total_planned_ltr.clear()
+                team._compute_current_liters_for_team()
+                total_current_ltr.append(team.current_liters)
+                
+                for user in team.member_ids:
+                    total_planned_ltr.append(user.planned_liters)
 
-        
+                team.update({'team_monthly_records_ids': [(0,0, {
+                    'team_id' : team.id,
+                    'planned_liters' : sum(total_planned_ltr),
+                    'current_liters' : sum(total_current_ltr),
+                    'registered_month' : datetime.datetime.now() + dateutil.relativedelta.relativedelta(months=-1)
+                })]})
+
+                team.write({'current_liters': 0})
+                team_ids.append(team.id)
