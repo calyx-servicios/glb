5a15d61e8efc885fc61682ec2c021b08a42f7ce2
gabbii <gperez@calyxservicios.com.ar>
2022-09-05 11:14:55 -0300

    [ODOO-4264][IMP][crm_invoicing_goal_liters][crm_changes_glb][oilnet_connector] - IMP module refactoring.

--- crm_invoicing_goal_liters/__manifest__.py
+++ crm_invoicing_goal_liters/__manifest__.py
@@ -1,29 +1,29 @@
 {
-
-    'name': "CRM Invoicing Goal Liters",
-
+    'name': "CRM invoicing goal liters",
     'summary': """
         CRM - Invoicing goal and liter counter
-        
     """,
-
     'description': """
         Module to add goal for sales equipment invoicing and liter counter per commercial user.
     """,
-
     "author": "Calyx Servicios S.A.",
-
-    "maintainers": ["DeykerGil"],
+    "maintainers": ["DeykerGil","PerezGabriela"],
     "website": "http://odoo.calyx-cloud.com.ar/",
     "license": "AGPL-3",
     "category": "Custom",
-    "version": "13.0.1.0.0",
-
-    'depends': ['base', 'crm', 'sale'],
-
+    "version": "13.0.2.0.0",
+    'depends': [
+        'crm', 
+        'sale',
+        'oilnet_connector'
+    ],
     'data': [
+        'data/ir_cron.xml',
+        'security/monthly_records_views_access_security.xml',
+        'security/ir.model.access.csv',
+        'views/monthly_records_views.xml',
         'views/crm_team_views.xml',
-        'views/res_users_views.xml'
+        'views/res_users_views.xml',
+        'views/sale_order_view.xml'
     ]
-
 }
