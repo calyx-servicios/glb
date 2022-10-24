1623a0b1845dfa7f4cde875277ac1b8b4e3b195d
gabbii <gperez@calyxservicios.com.ar>
2022-10-24 11:01:14 -0300

    [ODOO-5204][IMP][oilnet_connector][crm_label_competitors]

--- crm_label_competitors/models/crm_lead.py
+++ crm_label_competitors/models/crm_lead.py
@@ -3,5 +3,4 @@ from odoo import api, fields, models, _
 class Lead(models.Model):
     _inherit = "crm.lead"
     
-    competitors_tags = fields.Many2one('crm.label.competitors.registry', string=_('Competitors'))
-    
+    competitors_tags = fields.Many2one('crm.label.competitors.registry', string='Competitors')
