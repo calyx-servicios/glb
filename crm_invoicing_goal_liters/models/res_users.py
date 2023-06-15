b82071516dc0ffec28fa1d70927308cfd76b60e8
JaviZamoraDev <javier.zamora.dev@gmail.co>
2023-06-15 14:14:36 -0300

    [ODOO-6702][FIX][crm_invoicing_goal] - Fix range on date for get current liters

--- crm_invoicing_goal_liters/models/res_users.py
+++ crm_invoicing_goal_liters/models/res_users.py
@@ -8,25 +8,29 @@ class ResUsers(models.Model):
 
     def _compute_current_liters(self):
         """ Bring liters of sales orders that are of the "Fuel" category and belong to the team member. """
-        date = datetime.now() + relativedelta(months=-1)
+        today = datetime.now()
+        first_day_previous_month = datetime(today.year, today.month, 1) - timedelta(days=1)
+        first_day_previous_month = datetime(first_day_previous_month.year, first_day_previous_month.month, 1)
+        date = first_day_previous_month
+
         for rec in self:
             rec.current_liters = self.get_sum_values(date, rec)
-    
+
     current_liters = fields.Float("Current Liters",compute=_compute_current_liters)
     planned_liters = fields.Float("Planned Liters")
     user_monthly_records_ids = fields.One2many('res.users.monthly.records', 'res_user_id', 'Monthly records')
 
     def last_day_of_month(self, any_day):
         next_month = any_day.replace(day=28) + timedelta(days=4)
-        return next_month - timedelta(days=next_month.day)  
+        return next_month - timedelta(days=next_month.day)
 
     def get_sum_values(self,date, rec):
         last_day = self.last_day_of_month(date)
         values = []
         domain = [
             ('user_id','=', rec.id),
-            ('state', '=', 'logistics_auth'), 
-            ('authorized_date','>=',date.date()), 
+            ('state', '=', 'logistics_auth'),
+            ('authorized_date','>=',date.date()),
             ('authorized_date','<=',last_day.date())
         ]
         sale_orders = self.env['sale.order'].search(domain)
@@ -36,9 +40,13 @@ class ResUsers(models.Model):
                     values.append(line.product_uom_qty)
 
         return sum(values)
-    
+
     def cron_monthly_calculation_ig(self):
-        date = datetime.now() + relativedelta(months=-1)
+        today = datetime.now()
+        first_day_previous_month = datetime(today.year, today.month, 1) - timedelta(days=1)
+        first_day_previous_month = datetime(first_day_previous_month.year, first_day_previous_month.month, 1)
+        date = first_day_previous_month
+
         user_monthly_model = self.env['res.users.monthly.records']
         month_date = self.get_month_to_date(date) + " - " + str(date.year)
 
@@ -51,9 +59,9 @@ class ResUsers(models.Model):
 
     def get_month_to_date(self, date_period):
         dict_month = {
-            "1": _("January"), "2": _("February"), "3": _("March"), 
+            "1": _("January"), "2": _("February"), "3": _("March"),
             "4": _("April"), "5": _("May"), "6": _("June"),
-            "7": _("July"), "8": _("August"), "9": _("September"), 
+            "7": _("July"), "8": _("August"), "9": _("September"),
             "10": _("October"), "11": _("November"), "12":  _("December")
         }
         month = dict_month[str(date_period.month)]
@@ -63,9 +71,9 @@ class ResUsers(models.Model):
         for user in self:
             year = datetime.now().year
             dict_month = {
-                "1": _("January"), "2": _("February"), "3": _("March"), 
+                "1": _("January"), "2": _("February"), "3": _("March"),
                 "4": _("April"), "5": _("May"), "6": _("June"),
-                "7": _("July"), "8": _("August"), "9": _("September"), 
+                "7": _("July"), "8": _("August"), "9": _("September"),
                 "10": _("October"), "11": _("November"), "12":  _("December")
             }
             records = []
